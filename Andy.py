70c002aed90470be846ee1cf839d5e1c3ceb88b9
Andy-Mod <76206382+Andy-Mod@users.noreply.github.com>
2021-06-17 15:31:29 +0100

    Add files via upload

--- Andy.py
+++ Andy.py
@@ -32,3 +32,34 @@ Moy = (moyenne[0]+moyenne[1]+moyenne[2]+moyenne[3]+moyenne[4]+moyenne[5]+moyenne
 
 
 print(Moy)
+
+#EO
+L = list(range(1, 11))
+print(L)
+
+L[0]+=11
+L[1]+=11
+L[2]+=11
+L[3]+=11
+L[4]+=11
+L[5]+=11
+L[6]+=11
+L[7]+=11
+L[8]+=11
+L[9]+=11
+print(L)
+
+L.append(22)
+impair = L[1::2]
+pair =  L[0::2]
+print(pair, impair)
+L.extend([23, 24])
+L.pop(3)
+
+#E1
+d = {'nom':'Duplus', 'prenom':'Jacques', 'age':30}
+print(d.keys())
+print(d.values())
+print(d)
+phrase = d['prenom']+' '+d['nom']+' a '+ str(d['age'])+' ans.'
+print(phrase)
